d18c7377ef7b215a6adf2bdc504b5d266400b490
Gayatri Sawant <105240599+Gayatrii12@users.noreply.github.com>
2023-10-07 14:26:45 +0530

    first

---
+++ hby/src/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./Home.css"
+
+function Home(){
+    return (
+      <div className="Info">
+        <h1>What's Heliophysic Big Year?</h1>
+        <div className="i">
+          <h4>
+            The Heliophysics Big Year is a global celebration of solar science
+            and the Sun’s influence on Earth and the entire solar system. uring
+            the Heliophysics Big Year, you will have the opportunity to
+            participate in many solar science events such as watching solar
+            eclipses, experiencing an aurora, participating in citizen science
+            projects, and other fun Sun-related activities.
+          </h4>
+        </div>
+      </div>
+    );
+}
+
+export default Home;
